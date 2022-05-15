42f0bfa402ded0052539180dd830fef71d66d424
unknown <ulsa22992299@hotmail.com>
2022-05-15 14:23:00 +0300

    answer 3

--- answers.js
+++ answers.js
@@ -1,6 +1,21 @@
-// answer 1
+// /answer 1
 // let course = "midbar";
 // console.log(course);
-const age = 20;
-let age = 19;
-console.log(id);
+// /answer 2
+// const age = 20;
+// let age = 19;
+// console.log(id);
+let object =
+{
+    "name": "shon",
+    "age" : 18,
+    "likes" : "piano and computer games",
+    "dislikes": "mean people",
+    "phone": "054-111-and ain af ehad",
+    "favorite song": "baka-mitai",
+    "favorite wether": "spring",
+    "favorite drink": "water",
+    "best animal(not up for debate)": "cat",
+    "current phone": "xiomi 7",
+}
+console.log(object)
