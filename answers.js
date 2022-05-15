8dd3822d220808b3ff425441e4c0df44b6755412
unknown <ulsa22992299@hotmail.com>
2022-05-15 15:39:13 +0300

    answer 4

--- answers.js
+++ answers.js
@@ -5,17 +5,27 @@
 // const age = 20;
 // let age = 19;
 // console.log(id);
-let object =
-{
-    "name": "shon",
-    "age" : 18,
-    "likes" : "piano and computer games",
-    "dislikes": "mean people",
-    "phone": "054-111-and ain af ehad",
-    "favorite song": "baka-mitai",
-    "favorite wether": "spring",
-    "favorite drink": "water",
-    "best animal(not up for debate)": "cat",
-    "current phone": "xiomi 7",
+// /answer 3
+// let object =
+// {
+//     "name": "shon",
+//     "age" : 18,
+//     "likes" : "piano and computer games",
+//     "dislikes": "mean people",
+//     "phone": "054-111-and ain af ehad",
+//     "favorite song": "baka-mitai",
+//     "favorite wether": "spring",
+//     "favorite drink": "water",
+//     "best animal(not up for debate)": "cat",
+//     "current phone": "xiomi 7",
+// }
+// console.log(object)
+
+let number = 420;
+number = number*4;
+number = number -1;
+number = number/12;
+modulu = number/3;
+for(let index = 0; index < 6; index++){
+    console.log(number)
 }
-console.log(object)
